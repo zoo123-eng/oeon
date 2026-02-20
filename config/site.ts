80fde187431f44f2c2c1772bb6c5991b07e538a4
zoo123 <103585026+zoo123-eng@users.noreply.github.com>
2026-02-20 22:44:18 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { env } from "@/env.mjs";
 
 const site_url = env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 const email_r2_domain = env.NEXT_PUBLIC_EMAIL_R2_DOMAIN || "";
-const support_email = env.NEXT_PUBLIC_SUPPORT_EMAIL || "support@wr.do";
+const support_email = env.NEXT_PUBLIC_SUPPORT_EMAIL || "support@oeon.cc";
 const app_name = env.NEXT_PUBLIC_APP_NAME || "OEON";
 
 export const siteConfig: SiteConfig = {
@@ -14,43 +14,14 @@ export const siteConfig: SiteConfig = {
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
     twitter: "https://twitter.com/yesmoree",
-    github: "https://github.com/oiov/wr.do",
-    feedback: "https://github.com/oiov/wr.do/issues",
+    github: "https://github.com/oiov/wr.do", // 已保留原作者开源项目地址
+    feedback: "https://github.com/oiov/wr.do/issues", // 保留原项目反馈通道
     discord: "https://discord.gg/AHPQYuZu3m",
-    oichat: "https://oi.wr.do",
+    oichat: "https://oeon.cc",
   },
   mailSupport: support_email,
   emailR2Domain: email_r2_domain,
 };
 
-export const footerLinks: SidebarNavItem[] = [
-  {
-    title: "Company",
-    items: [
-      { title: "About", href: "/docs" },
-      { title: "Terms", href: "/terms" },
-      { title: "Privacy", href: "/privacy" },
-      { title: "Blog", href: "https://www.oiov.dev" },
-      { title: "Feedback", href: siteConfig.links.feedback },
-    ],
-  },
-  {
-    title: "Products",
-    items: [
-      { title: "LikeDo", href: "https://like.do" },
-      { title: "Vmail", href: "https://vmail.dev" },
-      { title: "Moise", href: "https://moise.oiov.dev" },
-      { title: "Iconce", href: "https://iconce.com" },
-      { title: "OiChat", href: siteConfig.links.oichat },
-    ],
-  },
-  {
-    title: "Docs",
-    items: [
-      { title: "Introduction", href: "/docs" },
-      { title: "Guide", href: "/docs/quick-start" },
-      { title: "Developer", href: "/docs/developer" },
-      { title: "Contact", href: siteConfig.mailSupport },
-    ],
-  },
-];
+// 保持空数组，即可在不破坏框架的前提下隐藏 Company, Products, Docs 三大板块
+export const footerLinks: SidebarNavItem[] = [];
