1fddcc5ffec6746a06cd3187ec847f015050d461
zoo123 <103585026+zoo123-eng@users.noreply.github.com>
2026-02-21 12:20:14 +0800

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,13 +1,19 @@
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+// 🔴 关键：必须确保这一行存在，否则会报“找不到名称 env”
+import { env } from "@/env.mjs"; 
+
 const linuxDoProvider: any = {
   id: "linuxdo",
   name: "OEON 论坛登录", 
   version: "2.0",
   type: "oauth",
-  // 加上 scope=basic 确保 WP 插件能识别
   authorization: "https://oeon.cc/oauth/authorize?scope=basic", 
   token: "https://oeon.cc/oauth/token",
   userinfo: "https://oeon.cc/wp-json/wp/v2/users/me",
-  // 临时直接把你的 ID 填在这里试试，看还会不会跳错
   clientId: env.LinuxDo_CLIENT_ID, 
   clientSecret: env.LinuxDo_CLIENT_SECRET,
   checks: ["state"],
@@ -21,3 +27,38 @@ const linuxDoProvider: any = {
     };
   },
 };
+
+export default {
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+    }),
+    linuxDoProvider, 
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        name: { label: "name", type: "text" },
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const res = await fetch(
+          process.env.AUTH_URL + "/api/auth/credentials",
+          {
+            method: "POST",
+            body: JSON.stringify(credentials),
+          },
+        );
+        if (res.ok) {
+          return res.json();
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
